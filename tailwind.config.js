538d23bebe549e710cb0fa69b9e4fdfeb344bc11
Ole Martin Larsen <ole@omlarsen.no>
2024-11-02 21:42:27 +0100

    feat: Basic login page with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/src/main/resources/templates/**/*.html',
+    './app/src/main/resources/static/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
