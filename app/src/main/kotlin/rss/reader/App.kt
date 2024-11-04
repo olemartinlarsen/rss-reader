c487971e98539a7af68096736a627c4ab90ddeef
Ole Martin Larsen <ole@omlarsen.no>
2024-11-04 19:34:01 +0100

    fix: Clean up imports

--- app/src/main/kotlin/rss/reader/App.kt
+++ app/src/main/kotlin/rss/reader/App.kt
@@ -4,17 +4,13 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.engine.*
-import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
-import kotlinx.serialization.Serializable
 import rss.reader.auth.UserSession
 import rss.reader.routes.authRoutes
-import rss.reader.templates.LoginTemplate
-import rss.reader.templates.DashboardTemplate
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
