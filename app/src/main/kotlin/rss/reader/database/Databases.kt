9eb5c8de53b4363aa086b1f6322a78a97047ef08
Ole Martin Larsen <ole@omlarsen.no>
2024-11-06 21:21:59 +0100

    feat: Update initDatabase with parameter for using a test database

--- app/src/main/kotlin/rss/reader/database/Databases.kt
+++ app/src/main/kotlin/rss/reader/database/Databases.kt
@@ -1,10 +1,8 @@
 package rss.reader.database
 
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
-import rss.reader.database.models.Users
 
-fun initDatabase() {
-    Database.connect("jdbc:sqlite:data.db", driver = "org.sqlite.JDBC")
+fun initDatabase(useTestDb: Boolean = false): Database {
+    val dbUrl = if (useTestDb) "jdbc:sqlite:test.db" else "jdbc:sqlite:data.db"
+    return Database.connect(dbUrl, driver = "org.sqlite.JDBC")
 }
